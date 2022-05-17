a90e696518c8a1355396c4af26f4d1b5cfdb748b
Demisov <lyricistsucks@gmail.com>
2022-05-17 18:00:17 +0300

    HomeWork1

---
+++ src/main/java/ru/gb/Denisov/HomeWorkApp.java
@@ -0,0 +1,50 @@
+package ru.gb.Denisov;
+;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords("Orange","Apple", "Banana");
+        printThreeWords();
+
+        if(checkSumSign())
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+
+        System.out.println(printColor());
+
+        if(compareNumbers())
+            System.out.println("a>=b");
+        else
+            System.out.println("a<b");
+    }
+    //Создайте метод printThreeWords(), который при вызове должен
+    // отпечатать в столбец три слова: Orange, Banana, Apple.
+
+    private static void printThreeWords(String s1, String s2,String s3){
+        System.out.println(s1+'\n'+s2+'\n'+s3+'\n' );
+    }
+    private static void printThreeWords(){
+        System.out.println("Orange"+'\n'+"Apple"+'\n'+"Banana");
+    }
+    private static boolean checkSumSign () {
+        int a = 3;
+        int b = -2;
+        return a+b>=0;
+    }
+    private static String printColor(){
+        int value =0;
+        if(value<=0)
+            return "Красный";
+        else if(value<100)
+            return "Желтый";
+        else
+            return "Зеленый";
+    }
+    private static boolean compareNumbers (){
+        int a=5;
+        int b =5;
+        return a>=b;
+    }
+}
+
