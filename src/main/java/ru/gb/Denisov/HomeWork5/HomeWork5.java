1012b706e6c92bb14a6609062b4411d43ae0c33c
Demisov <lyricistsucks@gmail.com>
2022-05-18 14:22:49 +0300

    HomeWork5

---
+++ src/main/java/ru/gb/Denisov/HomeWork5/HomeWork5.java
@@ -0,0 +1,27 @@
+package ru.gb.Denisov.HomeWork5;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+        Person s1 = new Person("Denisov Denis","Ingineer","DenisovDenis@@mailbox.com","8(954)266-11-98",4500,30);
+
+
+        Person[] arr = new Person[5];
+        arr[0] = new Person("Ivanov Ivan", "Ingineer", "IvanovIvan@mailbox.com","8(954)456-12-22" ,2000, 30);
+        arr[1] = new Person("Petrov Petr", "Booker","PetrovPetr@mailbox.com","8(954)568-22-22" ,2300, 56);
+        arr[2] = new Person("Sidorov Igor", "Ingineer", "SidorovIgor@mailbox.com","8(952)-364-56-22" ,2000, 23);
+        arr[3] = new Person("Filippov Filipp", "Head of Department","FilippovFilipp@mailbox.com","8(956)965-78-19" ,6500, 45);
+        arr[4] = new Person("Denisov Denis", "Assistant Engineer","DenisovDenis@mailbox.com","8(954)266-11-98" ,1500, 19);
+
+        System.out.println("Все сотрудники:");
+        for (int i =0; i< arr.length;i++){
+            arr[i].printInfo();
+        }
+        System.out.println();
+        System.out.println("Сотрудники старше 40 лет:");
+        for (int i =0; i< arr.length;i++){
+            if(arr[i].getAge()>40){
+                arr[i].printInfo();
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/gb/Denisov/HomeWork5/Person.java
@@ -0,0 +1,46 @@
+package ru.gb.Denisov.HomeWork5;
+
+public class Person {
+    private String fullName;
+    private String email;
+    private String position;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    Person (String fullName, String position, String email, String phoneNumber, int salary, int age){
+        this.fullName = fullName;
+        this.position=position;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.salary=salary;
+        this.age=age;
+    }
+
+    String getFullName (){
+        return this.fullName;
+    }
+    int getSalary(){
+        return this.salary;
+    }
+    int getAge(){
+        return this.age;
+    }
+    String getEmail(){
+        return this.email;
+    }
+    String getPosition(){
+        return this.position;
+    }
+    String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    void printInfo (){
+        System.out.println(getFullName()+" "+
+                getPosition()+" "+ getEmail()+" "+
+                getPhoneNumber()+" "+getSalary()+" "+
+                getAge());
+
+    }
+
+}
